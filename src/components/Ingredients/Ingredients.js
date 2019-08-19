e748dfa30ef2ea96ba45cd20a0e45bcc22ac0a47
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-08-19 13:40:14 +0200

    Initial commit

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS');
+  });
+
+  const filteredIngredients = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    setIsLoading(true);
+    fetch('https://react-hooks-5ac47.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = id => {
+    setIsLoading(true);
+    fetch(`https://react-hooks-5ac47.firebaseio.com/ingredients/${id}.json`, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        setIsLoading(false);
+        setUserIngredients(prevIngredients =>
+          prevIngredients.filter(ingredient => ingredient.id !== id)
+        );
+      })
+      .catch(err => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  };
+
+  return (
+    <div className='App'>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
+      <section>
+        <Search onLoadIngredients={filteredIngredients} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
